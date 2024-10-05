6e8ebf7bc9788c555ace6e07b830cacf2758c2e1
number one cyber developer. <146389741+archescyber@users.noreply.github.com>
2024-10-05 13:33:51 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,58 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin, urlparse
+import os
+import platform
+
+def temizle():
+    os_turu = platform.system()
+
+    if os_turu == "Windows":
+        os.system("cls")
+    elif os_turu == "Linux" or os_turu == "Darwin":
+        os.system("clear") 
+    else:
+        print("[<>] The purge command is not supported for this operating system.")
+print("")
+print("""
+
+             d8888          d8b                    
+            d88888          Y8P                    
+           d88P888                                 
+          d88P 888 88888b.  888                    
+         d88P  888 888 "88b 888                    
+        d88P   888 888  888 888                    
+       d8888888888 888 d88P 888                    
+      d88P     888 88888P"  888                    
+                   888                             
+                   888                             
+                   888                             
+88888888888                888                     
+    888                    888                     
+    888                    888                     
+    888   .d88b.  .d8888b  888888  .d88b.  888d888 
+    888  d8P  Y8b 88K      888    d8P  Y8b 888P"   
+    888  88888888 "Y8888b. 888    88888888 888     
+    888  Y8b.          X88 Y88b.  Y8b.     888     
+    888   "Y8888   88888P'  "Y888  "Y8888  888     
+                                                   
+""")                                                   
+                                                   
+# Temizleme fonksiyonunu çağır
+def fetch_api_response(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        return response.json()  # Return the JSON response
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+
+if __name__ == "__main__":
+    api_url = input("Please enter the API URL: ")
+    result = fetch_api_response(api_url)
+
+    if result is not None:
+        print("API Response:")
+        print(result)
